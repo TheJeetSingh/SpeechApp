1a4335f609de8d07e1ca20107d118cd9dd5e5cdc
Jeet Singh <4lifeinstantkarma@gmail.com>
2025-02-22 21:14:53 -0800

    Analytics is out

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 function HomeScreen() {
   const navigate = useNavigate();
