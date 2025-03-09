75eeec377d69999285878d0785eb81e5f25bd912
Jeet Singh <4lifeinstantkarma@gmail.com>
2025-03-08 22:25:20 -0800

    fixed index.js

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5001;
 // CORS Configuration
 const allowedOrigins = [
   "http://localhost:3000", // Local development
-  "https://speech-app-delta.vercel.app/*", // Vercel frontend deployment
+  "https://speech-app-delta.vercel.app/signup",
+  "https://speech-app-delta.vercel.app/login", // Vercel frontend deployment
 ];
 
 const corsOptions = {
